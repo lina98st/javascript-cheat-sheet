60c85870be88bbb203b16905914abfceb685531d
Alina <156576799+lina98st@users.noreply.github.com>
2025-01-09 14:23:10 +0100

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,7 @@
 // Select the button element
 let button = document.querySelector("button");
 
-// Add an event listener for the click event
+// Add an event listener for the 'click' event
 button.addEventListener("click", function () {
     // Toggle the 'anzeigen' class on the button
     button.classList.toggle("anzeigen");
