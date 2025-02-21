3c97ccbb203e3ea23fe621ef1d66dddd717e246c
Alina <156576799+lina98st@users.noreply.github.com>
2025-02-21 09:34:41 +0100

    Create arrayMethoden.js

---
+++ arrayMethoden.js
@@ -0,0 +1,6 @@
+let fruits = ["Apfel", "Banane", "Kirsche"];
+
+fruits.push("Orange"); // Fügt am Ende hinzu
+fruits.pop(); // Entfernt das letzte Element
+fruits.shift(); // Entfernt das erste Element
+fruits.unshift("Mango"); // Fügt am Anfang hinzu
